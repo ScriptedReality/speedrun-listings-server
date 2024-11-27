7100495b720901ba5dbaa00b4afd2c999e915590
Christian Toney <christian@makuwro.com>
2024-11-27 10:46:27 -0500

    Init

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import accountsRouter from "./routes/accounts.js";
+import runsRouter from "./routes/runs.js";
+import gamePagesRouter from "./routes/game-pages.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.disable("x-powered-by");
+app.use("/runs", runsRouter);
+app.use("/accounts", accountsRouter);
+app.use("/game-pages", gamePagesRouter);
+
+app.get("/", (_, response) => response.json({success: true}));
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`\x1b[32mNow listening on port ${port}.\x1b[0m`));
