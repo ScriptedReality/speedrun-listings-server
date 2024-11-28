2b461331d55f4cc48bf3fe9f0445cc4fcee0f1fa
Christian Toney <christian@makuwro.com>
2024-11-27 22:09:23 -0500

    Create a new endpoint: POST /game-pages/:gamePageID

--- src/routes/game-pages.ts
+++ src/routes/game-pages.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import gamePageIDRouter from "./game-pages/[gamePageID].js"
 
 const router = Router();
+router.use("/:gamePageID", gamePageIDRouter);
 
 export default router;
---
+++ src/routes/game-pages/[gamePageID].ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/:gamePageID", async () => {
+
+});
+
+export default router;
