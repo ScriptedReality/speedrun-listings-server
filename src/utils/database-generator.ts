4c317885ef374623baf39f42e88226fef075d2f9
Christian Toney <christian@makuwro.com>
2024-11-29 18:12:09 -0500

    Update MongoDB driver

--- src/utils/database-generator.ts
+++ src/utils/database-generator.ts
@@ -1,6 +1,11 @@
 import { MongoClient } from "mongodb";
 
-const mongoClient = new MongoClient("");
+const mongoURI = process.env.MONGODB_URI;
+if (!mongoURI) throw new Error("Please define MONGODB_URI in the .env file.");
+
+const mongoClient = new MongoClient(mongoURI, {
+  tlsCertificateKeyFile: "./etc/secrets/mongodb.pem"
+});
 const database = mongoClient.db();
 
 export default database;
