b95dc512563b4eeb1892fcdbee85ecca87a22e7b
Christian Toney <christian@makuwro.com>
2024-12-04 13:07:28 -0500

    Confirm email address, username, and password

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -2,9 +2,17 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/", async () => {
+router.post("/", async (request, response) => {
 
-  
+  // Verify that an email address, username, and password were provided. 
+  const {emailAddress, username, password} = request.body;
+  if (!emailAddress || !username || !password) {
+
+    return response.status(400).json({
+      message: `A${!emailAddress ? "n email address" : (!username ? " username" : " password")} is required to create an account.`
+    });
+
+  }
 
 });
 
