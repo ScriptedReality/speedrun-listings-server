c548fcf8cbcd853cfbf24584ca5166cf6bb5e2f8
Christian Toney <christian@makuwro.com>
2024-12-05 20:57:26 -0500

    Update CORS (#33)

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,7 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 app.disable("x-powered-by");
 app.use("/runs", runsRouter);
 app.use("/account", accountRouter);
