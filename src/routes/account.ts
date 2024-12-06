4f00072927d72af8355709888476b0879e0a25dd
Christian Toney <christian@makuwro.com>
2024-12-05 23:33:44 -0500

    Add route to get account data

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,7 +1,23 @@
 import { Router } from "express";
 import sessionsRouter from "./account/sessions.js";
+import authenticator from "#utils/authenticator.js";
 
 const router = Router();
 router.use("/sessions", sessionsRouter);
+
+router.get("/", authenticator);
+router.get("/", async (request, response) => {
+
+  const accountData: {[key: string]: unknown} = {};
+
+  for (const key of response.locals.accountData) {
+
+    accountData[key === "_id" ? "accountID" : key] = response.locals.accountData[key];
+
+  }
+
+  response.json(accountData);
+
+});
 
 export default router;
